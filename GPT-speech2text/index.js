864af34fb13a68c677e5d8ca2e783f1dbad301ff
Cody <ken51717@gmail.com>
2023-11-16 22:00:30 +0800

    feat:新增文字下載功能，即加上語音段落

--- GPT-speech2text/index.js
+++ GPT-speech2text/index.js
@@ -34,6 +34,7 @@ const transcribeAudio = () => {
 
     formData.append('file', audioInput.files[0])
     formData.append('model', 'whisper-1')
+    formData.append('response_format', 'srt')
 
 
     axios
@@ -44,9 +45,10 @@ const transcribeAudio = () => {
             }
         })
         .then((response) => {
-            transcriptionText = response.data.text
+            const srtResult = response.data; // 假設API回傳了SRT格式的數據
 
-            transcriptionResult.textContent = transcriptionText
+            const formattedText = formatSrtToDisplay(srtResult);
+            transcriptionResult.textContent = formattedText; // 這裡可能需要根據SRT格式來進行不同的處理
         })
         .catch((error) => {
 
@@ -56,9 +58,11 @@ const transcribeAudio = () => {
 
 const summarizeText = () => {
     const summaryResult = document.getElementById('markdown')
-    const transcriptionResult = document.getElementById('translation')
-
-    if (!transcriptionResult) {
+    // 假設transcriptionResult.value包含時間戳和文本
+    const transcriptionResult = document.getElementById('translation').value;
+    // 使用正則表達式替換時間戳部分為空字串
+    const textWithoutTimestamps = transcriptionResult.replace(/\[\d{2}:\d{2}:\d{2} - \d{2}:\d{2}:\d{2}\]/g, '');
+    if (!textWithoutTimestamps) {
         alert('沒有轉錄文字可總結。')
         return
     }
@@ -79,14 +83,13 @@ const summarizeText = () => {
     5. Proficient in using markdown tables to collect information and help users better understand the target information.
     6. If the user does not specify any language, then default to using Chinese for the reply.
     7. Please do not worry about your response being interrupted, try to output your reasoning process as much as possible.
-    8. 請對以下內容進行。並用繁體中文回覆。
-    9. 並透過後設提問提供思維的收束，嘗試在回覆中找到多個論點，並統整這些論點。
-    10. 使用marldown表格，來說明你的論點
+    8. 請對以下內容進行分析，分析以下內容重點。並用繁體中文回覆。
+    9. 並透過後設提問提供思維的收束，嘗試在回覆中找到多個論點，並統整這些論點，整理成一份markdown回覆。
     `
                 },
                 {
                     role: 'user',
-                    content: transcriptionResult ? transcriptionResult.value : ''
+                    content: textWithoutTimestamps ? textWithoutTimestamps : ''
                 }
             ]
         },
@@ -110,3 +113,59 @@ const summarizeText = () => {
 
 document.getElementById('translate-btn').addEventListener('click', transcribeAudio)
 document.getElementById('convert-md-btn').addEventListener('click', summarizeText)
+
+
+function formatSrtToDisplay(srtText) {
+    // 使用正則表達式分割每個字幕塊
+    const blocks = srtText.trim().split(/\n\n+/);
+
+    // 轉換每個字幕塊為新格式
+    return blocks.map(block => {
+        // 分割每一塊的行
+        const lines = block.split('\n');
+
+        // 忽略序號，只處理時間戳和字幕文本
+        const times = lines[1];
+        const text = lines.slice(2).join(' '); // 如果字幕文本跨多行則合併
+
+        // 提取開始和結束時間，並去除毫秒
+        const [start, end] = times.split(' --> ').map(time => time.substr(0, 8));
+
+        // 構建新格式的字串
+        return `[${start} - ${end}]:${text}`;
+    }).join('\n');
+}
+
+//下載txt檔案
+document.addEventListener('DOMContentLoaded', function () {
+    const exportButton = document.getElementById('exportButton');
+    const transcriptionDiv = document.getElementById('translation');
+    const downloadLink = document.getElementById('downloadLink');
+
+    exportButton.addEventListener('click', function () {
+        // 獲取轉錄文本
+        const transcriptionText = transcriptionDiv ? transcriptionDiv.textContent : '';
+
+        // 檢查是否有轉錄文本
+        if (!transcriptionText.trim()) {
+            alert('沒有轉錄文字可以下載！');
+            return;
+        }
+
+        // 創建Blob對象
+        const blob = new Blob([transcriptionText], { type: 'text/plain' });
+
+        // 創建URL以下載文件
+        const url = URL.createObjectURL(blob);
+
+        // 設置下載鏈接的URL和文件名
+        downloadLink.href = url;
+        downloadLink.style.display = 'block';
+
+        // 单击下載鏈接以觸發下載
+        downloadLink.click();
+
+        // 釋放URL對象
+        URL.revokeObjectURL(url);
+    });
+});
